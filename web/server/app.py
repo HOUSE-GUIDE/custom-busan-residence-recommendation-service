001fd0a7ca33105074aeef91daacbe6898a2b0c5
Yujin <qrt365@naver.com>
2023-12-02 22:13:59 +0900

    fixed: 정적 파일 경로 수정

--- web/server/app.py
+++ web/server/app.py
@@ -10,9 +10,9 @@ def index():
 @app.route('/recommend', methods=['POST'])
 def recommend():
     user_weights = request.form.to_dict()
-    user_weights = {k: int(v) for k, v in user_weights.items()}  # 입력은 문자열이므로 숫자로 변환해줍니다.
+    user_weights = {k: int(v) for k, v in user_weights.items()}
 
-    top3_dong = average.calculate_similarity(user_weights)  # average.py에서 함수를 호출하여 결과를 얻습니다.
+    top3_dong = average.calculate_similarity(user_weights)
 
     return render_template('result.html', top3_dong=top3_dong)
 
